6948a718ba8746ae8e8d7e5aaf5acb9cfcfc76f7
Tt <cntang@live.cn>
2019-05-24 17:13:07 +0800

    修改数据库配置

--- data/config/database.ini.php
+++ data/config/database.ini.php
@@ -1,10 +1,10 @@
 <?php
 if (!defined('IN_XIAOCMS')) exit();
 return array (
-  'host' => '192.168.27.205',
-  'username' => 'root',
-  'password' => '',
-  'dbname' => 'ice22',
+  'host' => '47.106.193.227',
+  'username' => 'icepoint',
+  'password' => 'vy7Qad15Vy4r0qWgbQ9WZBxr',
+  'dbname' => 'icepoint',
   'prefix' => 'xiao_',
   'port' => '3306',
   'charset' => 'utf8',
