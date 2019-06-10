8a17e9e23e12ed8afe842a5601d1d063a4410173
唐田 <cntang@live.cn>
2019-06-10 22:51:35 +0800

    更新激活逻辑

--- core/controller/ip.php
+++ core/controller/ip.php
@@ -3,7 +3,7 @@
 class ip extends Base
 {
 	// 当前用户信息
-	private $user = null;
+	protected $user = null;
 
 	public function __construct()
 	{
@@ -11,15 +11,15 @@ class ip extends Base
 
 		$token = $_SERVER['HTTP_TOKEN'];
 
-		$user = $this->cache->get('ip:' . $token);
-		if ($user) {
+		$this->user = $this->cache->get('ip:' . $token);
+		if ($this->user) {
 			return;
 		}
 
 		$loginInfo = $this->cache->get('wx:' . $token);
 		if ($loginInfo) {
-			$user = $this->db->setTableName('customer') . getOne('openid = ?', $loginInfo['openid']);
-			$this->cache->set('ip:' . $token, $user);
+			$this->user = $this->db->setTableName('customer')->getOne('openid = ?', $loginInfo['openid']);
+			$this->cache->set('ip:' . $token, $this->user);
 			return;
 		}
 
@@ -27,10 +27,17 @@ class ip extends Base
 	}
 
 	// 激活卡券
-	public function activecarddAction()
+	public function activecardAction()
 	{
+		$uts = $this->cache->get('useractivetimes:' . $this->user['id']);
+		if ($uts > 100) {
+			$this->json(null, false, '检测到风险操作，请24小时后重试');
+		}
+		$uts += 1;
+		$this->cache->set('useractivetimes:' . $this->user['id'], $uts, 3600 * 24);
+
 		$card = null;
-		$qrcode = $this->post('qrcode');
+		$qrcode = null; //  暂时禁用二维码激活 $this->post('qrcode');
 		if ($qrcode) {
 			$card = $this->db->setTableName('card')->getOne('qrcode = ?', $qrcode);
 		} else {
@@ -39,10 +46,24 @@ class ip extends Base
 			if (empty($code) || empty($pass)) {
 				$this->json(null, false, '卡号或者密码为空');
 			}
+			$times = $this->cache->get('cardactivetimes:' . $code);
+			if ($times > 10) {
+				$this->json(null, false, '卡券激活次数超出限制，请24小时后重试');
+			}
+			$times += 1;
+			$this->cache->set('cardactivetimes:' . $code, $times, 3600 * 24);
 			$card = $this->db->setTableName('card')->getOne('code = ? and pass = ?', [$code, $pass]);
 		}
 
 		if ($card) {
+			switch ($card['status']) {
+				case 10:
+					$this->json(null, false, '未销售的卡券不能激活');
+				case 30:
+					$this->json(null, false, '已经激活的卡券不能激活');
+				case 40:
+					$this->json(null, false, '已作废的卡券不能激活');
+			}
 			$ct = $this->db->setTableName('card_type')->getOne('id = ?', $card['cardtypeid']);
 			if (!$ct) {
 				$this->json(null, false, '卡券类型不存在，不能激活');
@@ -70,7 +91,12 @@ class ip extends Base
 				$this->json(null, false, '激活卡券明细失败，请重试');
 			}
 			// 设置卡券激活用户信息
-			if (!$this->db->setTableName('card')->update(['customermobile' => $this->user['mobile'], 'customerid' => $this->user['id']], 'id = ?', $card['id'])) {
+			if (!$this->db->setTableName('card')->update([
+				'customermobile' => $this->user['mobile'],
+				'customerid' => $this->user['id'],
+				'status' => 30,
+				'activetime' => time()
+			], 'id = ?', $card['id'])) {
 				$this->db->setTableName('card_item')->delete('cardid = ?', $card['id']);
 				$this->json(null, false, '激活卡券失败，请重试');
 			} else {
