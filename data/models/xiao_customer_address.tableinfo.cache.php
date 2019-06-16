e78be3c3af54af139e3492929bfb07b98e2894ad
唐田 <cntang@live.cn>
2019-06-16 12:49:57 +0800

    添加缓存文件

---
+++ data/models/xiao_customer_address.tableinfo.cache.php
@@ -0,0 +1,18 @@
+<?php
+if (!defined('IN_XIAOCMS')) exit();
+return array (
+  'primaryKey' => 'id',
+  'fields' => 
+  array (
+    0 => 'id',
+    1 => 'customerid',
+    2 => 'province',
+    3 => 'city',
+    4 => 'area',
+    5 => 'address',
+    6 => 'isDefault',
+    7 => 'createtime',
+    8 => 'mobile',
+    9 => 'name',
+  ),
+);
